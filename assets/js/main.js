e87a38f9aa7a34fe6fd60f7c2f02a353a9908158
Mitch Brudy <mitchbrudel@gmail.com>
2019-02-05 20:11:18 -0500

    2.5 8:11 CHANGES

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 $('.ui.dropdown')
-    .dropdown({
-        values: [
-            {
-                name: 'Male',
-                value: 'male'
-            },
-            {
-                name     : 'Female',
-                value    : 'female',
-                selected : true
-            }
-        ]
-    })
+  .dropdown({
+    values: [
+      {
+        name: 'Male',
+        value: 'male'
+      },
+      {
+        name     : 'Female',
+        value    : 'female',
+        selected : true
+      }
+    ]
+  })
 ;
